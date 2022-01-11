c6ea514e31dd88d31afd027dd95d8bedee7a0f09
Ranjith Kumar <kranjith490@gmail.com>
2022-01-11 10:21:51 +0530

    removed cros origin issues

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const axios = require("axios");
-const { response } = require("express");
-const { json } = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/api/countries", (req, res) => {
   axios
